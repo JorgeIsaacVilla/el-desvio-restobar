914808c255b28827d7656d76e0410ed7f43c683e
jorgeisaacvilla <jvilla10@outlook.es>
2023-12-11 20:23:04 -0500

    Actualizacion de fotos combo. se desbloque la opcion de imagenes en banner.

--- src/components/Body/ArrayMenu/Menu14.js
+++ src/components/Body/ArrayMenu/Menu14.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Menu.css";
-/*import imagen1 from "./imgMenu14/img1.jpg";
+import imagen1 from "./imgMenu14/img1.jpg";
 import imagen2 from "./imgMenu14/img2.jpg";
 import imagen3 from "./imgMenu14/img3.jpg";
-import imagen4 from "./imgMenu14/img4.jpg";*/
+/*import imagen4 from "./imgMenu14/img4.jpg";*/
 
 /*Por solicitud del cliente este menú se mostrará de lunes a jueves */
 
@@ -15,19 +15,19 @@ function Menu14({sectionId, onProductClick}){
 
     const menuItem =[
     {pos:1,
-    image:"imagen1",
+    image:imagen1,
     food:"Combo Personal Perro Caliente",
     price:11000,
     ingredients:"Perro Sencillo + 100gr de Papas + Gaseosa PET 250ml Postobón"},
 
     {pos:2,
-    image:"imagen2",
+    image:imagen2,
     food:"Combo Personal Hamburguesa",
     price:23000,
     ingredients:"Hamburguesa de Pollo ó Carne, 100gr de papas + gaseosa PET 250ml Postobón"},
 
     {pos:3,
-    image:"imagen3",
+    image:imagen3,
     food:"Combo Para 2",
     price:25000,
     ingredients:"Una salvajada + Choributy + 200gr de Papas + 2 gaseosas PET 250ml Postobón"},
@@ -39,7 +39,7 @@ function Menu14({sectionId, onProductClick}){
                 {menuItem.map((item)=>
                 <li key= {item.pos} onClick={() => handleItemclick(item)}>
                     <div className="item-contend">
-                        {/*<img src={item.image} />*/}
+                        <img src={item.image} />
                         <div>
                             <h3 
                              className="tittle-food">
